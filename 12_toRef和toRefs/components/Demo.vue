abd478d4f5deab8214e23ce43e249aea680e4197
xionghaiying <1163702239@qq.com>
2023-09-01 17:48:00 +0800

    feat[src]:toRef和toRefs，shallowReactive和shallowRef

--- 12_toRef和toRefs/components/Demo.vue
+++ 12_toRef和toRefs/components/Demo.vue
@@ -1,22 +1,19 @@
 <template>
   <h2>姓名：{{name}}</h2>
   <h2>年龄：{{age}}</h2>
-  <h2>数字：{{job.one.salary}}</h2>
+  <h2>数字：{{salary}}</h2>
   <button @click="name += '~~' ">修改姓名</button>
   <button @click="age += 1">修改年龄</button>
-  <button @click="job.one.salary++">修改数字</button>
-  <hr>
-  <h2>当前的sum.a值是：{{sum.a}}</h2>
-  <button @click="sum.a++ ">点位sum.a加1</button>
+  <button @click="salary++">修改数字</button>
 </template>
 
 <script>
-  import { ref,reactive,toRef,toRefs,shallowReactive,shallowRef } from "vue";
+  import { ref,reactive,toRef,toRefs } from "vue";
   export default {
     name: 'Demo',
     setup(){
-      // 数据 
-      let person = shallowReactive({
+      // 数据 如果有计算属性，需要在代理对象之前定义
+      let person = reactive({
         name: '熊海鹰',
         age: 29,
         job:{
@@ -26,16 +23,14 @@
         }
       })
 
-      let sum = shallowRef({
-        a:0
-      })
-      console.log(sum);
-
+      let refPerson = toRefs(person)
+      console.log('%%%%%',toRefs);
       // 返回一个对象（常用）
       return {
-        sum,
-        person,
-        ...toRefs(person)
+        name: toRef(person,'name'),
+        age: toRef(person,'age'),
+        salary: toRef(person.job.one,'salary'),  
+         
       }
     },
   }
